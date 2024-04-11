284d08c1ef40894dcf4d3db8b84e92d149592921
r4bb1t <r4bb1t.github@gmail.com>
2024-04-11 14:43:02 +0900

    style: sidebar button hover style

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -4,8 +4,8 @@ export default function Sidebar() {
   return (
     <aside className="top-0 flex min-h-0 w-80 flex-col border-r">
       <h1 className="flex p-8 text-xl font-extrabold">서비스 이름</h1>
-      <ul className="h-full w-full px-8">
-        <li className="group flex items-center gap-2 font-medium hover:bg-black/5">
+      <ul className="h-full w-full">
+        <li className="flex cursor-pointer items-center gap-2 px-8 py-2 font-medium hover:bg-black/5">
           <HiOutlineDocumentText size={28} />
           <div className="line-clamp-1">
             [보건자료처리] 기술통계 및 자료의 기초
