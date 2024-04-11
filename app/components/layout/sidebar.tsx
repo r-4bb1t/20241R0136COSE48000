ec92bd2ef2ca5561d8383cad149d519811ce8a7d
r4bb1t <r4bb1t.github@gmail.com>
2024-04-11 14:42:10 +0900

    feat: add sidebar ui (wip)

---
+++ app/components/layout/sidebar.tsx
@@ -0,0 +1,20 @@
+import { HiOutlineDocumentText } from "react-icons/hi";
+
+export default function Sidebar() {
+  return (
+    <aside className="top-0 flex min-h-0 w-80 flex-col border-r">
+      <h1 className="flex p-8 text-xl font-extrabold">서비스 이름</h1>
+      <ul className="h-full w-full px-8">
+        <li className="group flex items-center gap-2 font-medium hover:bg-black/5">
+          <HiOutlineDocumentText size={28} />
+          <div className="line-clamp-1">
+            [보건자료처리] 기술통계 및 자료의 기초
+          </div>
+        </li>
+      </ul>
+      <button className="w-full border-t py-4 font-semibold hover:bg-black/5">
+        강의 음성 올리기
+      </button>
+    </aside>
+  );
+}
